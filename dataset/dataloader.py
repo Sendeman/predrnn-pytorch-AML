6a13a0bf5c71714f63f5cf8530ec9ef2bc513290
PaulusBoskabouter <paul_2000@hotmail.nl>
2025-05-16 13:25:32 +0200

    Update .gitignore and enhance dataloader: add KTHDataset and KTHDatasetRNN classes with data loading functionality

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -1,6 +1,55 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+from pathlib import Path
 
 
 ## TODO DATALOADER STUFFIES.
+
+class KTHDataset(Dataset):
+    def __init__(self, root_dir: str, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.data = []
+        self.labels = []
+        self.load_data()
+
+    def load_data(self):
+        # Load the data from the directory and populate self.data and self.labels
+        pass
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        label = self.labels[idx]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, label
+    
+class KTHDatasetRNN(Dataset):
+    def __init__(self, root_dir: Path = Path("dataset\KTH_data"), transform=None):
+        self.root_dir = root_dir 
+        self.transform = transform
+        self.videos = []
+        self.load_data()
+
+    def load_data(self):
+        # Load the data from the directory and populate self.data and self.labels
+        ...
+        
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        label = self.labels[idx]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, label
