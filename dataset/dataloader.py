42f8f97aae27f3942dd548152af3c9c45806a76a
PaulusBoskabouter <paul_2000@hotmail.nl>
2025-05-16 14:01:25 +0200

    Refactor KTHDataset class: update constructor to accept file paths and action labels, implement data loading method, and adjust item retrieval logic.

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -1,34 +1,32 @@
 import torch
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from pathlib import Path
 
 
-## TODO DATALOADER STUFFIES.
-
 class KTHDataset(Dataset):
-    def __init__(self, root_dir: str, transform=None):
-        self.root_dir = root_dir
+    def __init__(self, file_paths:np.array, action_labels:np.array, transform=None):
         self.transform = transform
-        self.data = []
-        self.labels = []
-        self.load_data()
+        self.paths = file_paths
+        self.labels = action_labels
 
-    def load_data(self):
-        # Load the data from the directory and populate self.data and self.labels
-        pass
+
+    def load_data(self, file_loc):
+        return torch.load(file_loc)
 
     def __len__(self):
-        return len(self.data)
+        return len(self.paths)
 
     def __getitem__(self, idx):
-        sample = self.data[idx]
+        sample = self.load_data(self.paths[idx])
         label = self.labels[idx]
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample, label
+   
     
 class KTHDatasetRNN(Dataset):
     def __init__(self, root_dir: Path = Path("dataset\KTH_data"), transform=None):
